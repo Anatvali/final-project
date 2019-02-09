cf02fbdc502476ec65fb4d6794bf53385b6aa62c
Anatvali <ana.tvaliashvili@geolab.edu.ge>
2019-02-09 22:51:25 +0400

    new update

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+const autoprefixer = require('gulp-autoprefixer');
+// var cleancss = require('gulp-cleancss');
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
   return gulp.src('scss/style.scss')
     .pipe(sass().on('error', sass.logError))
+    // .pipe(cleancss({keepBreakes: false}))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
     .pipe(gulp.dest('styles'));
 });
 
